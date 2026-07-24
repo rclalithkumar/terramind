e0f66b868d3f0bd75906e41db4cbf27dd24942a1
rclalithkumar <rclalith.amcec@gmail.com>
2026-07-25 00:11:49 +0530

    added interactive globe

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -13,7 +13,7 @@ export default function Container({
   return (
     <div
       className={clsx(
-        "mx-auto w-full max-w-[1440px] px-6 md:px-8 lg:px-12",
+        "mx-auto w-full max-w-360 px-6 md:px-8 lg:px-12",
         className
       )}
     >
